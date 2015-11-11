bb3ec164b14ab5f85dd79559e62c22025ac89ed1
snowman1ng <tue99667@temple.edu>
2015-11-10 20:39:23 -0500

    Adds some functionality for the crossfade bar. The values needed for Song A and Song B are now displayed in input fields as the bar is moved. Bar is not yet linked to a SoundManager2 object.

--- testing/crossfade.js
+++ testing/crossfade.js
@@ -1,17 +1,29 @@
-$(function() {
-    $( "#slider" ).slider();
-    min: 0
-    max: 100
-  });
+//set variable reference to the slider
+var slider = $( "#slider");
 
-$( "#selector" ).on( "slidechange", function( event, ui ) {
-	var value = $( "#selector" ).slider( "option", "value" );
-	var fake = $("#crossfade-current-num");
-	fake.text(value);
-} );
-
+//make the div into a slider, set its default values, add event listeners which 
+//update an input field based on the value of the slider
 $(function() {
-var value = $( "#selector" ).slider( "option", "value" );
-var fake = $("#crossfade-current-num");
-fake.text(value);
+    $("#slider").slider({
+    	min: 0,
+    	max: 100,
+    	step: 1,
+    	value: 0,
+    	start: function( event, ui ) {
+					$("#crossfade-current-numA").val(100 - ui.value);
+					$("#crossfade-current-numB").val(ui.value);
+               },
+        stop: function( event, ui ) {
+                  $("#crossfade-current-numA").val(100 - ui.value);
+                  $("#crossfade-current-numB").val(ui.value);
+               },
+        change: function( event, ui ) {
+                  $("#crossfade-current-numA").val(100 - ui.value);
+                  $("#crossfade-current-numB").val(ui.value);
+               },
+        slide: function( event, ui ) {
+                  $("#crossfade-current-numA").val(100 - ui.value);
+                  $("#crossfade-current-numB").val(ui.value);
+               }
+    });
 });
