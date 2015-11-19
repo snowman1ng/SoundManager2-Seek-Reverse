2f142dae54f2b68e4642516cfc27060dbaed96a6
snowman1ng <tue99667@temple.edu>
2015-11-18 23:15:29 -0500

    Added z-index to ensure black keys appear in front of white keys. Also adds javascript to change the color when a key is clicked.

---
+++ testing/scripts/keyboard-piano.js
@@ -0,0 +1,57 @@
+
+$( document ).ready(function() {
+	var whiteKey = $( ".white-key" );
+	var blackKey = $( ".black-key" );
+
+	$( "#key-one").click(function() {
+		$( "#key-one").css( "background-color", "pink");
+	});
+
+	$( "#key-two").click(function() {
+		$( "#key-two").css( "background-color", "pink");
+	});
+
+	$( "#key-three").click(function() {
+		$( "#key-three").css( "background-color", "pink");
+	});
+
+	$( "#key-four").click(function() {
+		$( "#key-four").css( "background-color", "pink");
+	});
+
+	$( "#key-five").click(function() {
+		$( "#key-five").css( "background-color", "pink");
+	});
+
+	$( "#key-six").click(function() {
+		$( "#key-six").css( "background-color", "pink");
+	});
+
+	$( "#key-seven").click(function() {
+		$( "#key-seven").css( "background-color", "pink");
+	});
+
+	$( "#key-eight").click(function() {
+		$( "#key-eight").css( "background-color", "pink");
+	});
+
+	$( "#key-nine").click(function() {
+		$( "#key-nine").css( "background-color", "pink");
+	});
+
+	$( "#key-ten").click(function() {
+		$( "#key-ten").css( "background-color", "pink");
+	});
+
+	$( "#key-eleven").click(function() {
+		$( "#key-eleven").css( "background-color", "pink");
+	});
+
+	$( "#key-twelve").click(function() {
+		$( "#key-twelve").css( "background-color", "pink");
+	});
+
+	
+
+	function trigger() {};
+});
