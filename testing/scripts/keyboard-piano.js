c2cc50d2d774b672f462374ee924046920589801
snowman1ng <tue99667@temple.edu>
2015-11-18 23:43:03 -0500

    Just makes keyboard keys turn pink for a second when clicked to ensure that javascript events will work on the keys.

--- testing/scripts/keyboard-piano.js
+++ testing/scripts/keyboard-piano.js
@@ -1,57 +1,97 @@
-
 $( document ).ready(function() {
-	var whiteKey = $( ".white-key" );
-	var blackKey = $( ".black-key" );
-
 	$( "#key-one").click(function() {
 		$( "#key-one").css( "background-color", "pink");
+		setTimeout(function(){ 
+			$( "#key-one").css( "background-color", "white");
+			}, 1000
+		);
 	});
 
 	$( "#key-two").click(function() {
 		$( "#key-two").css( "background-color", "pink");
+		setTimeout(function(){ 
+			$( "#key-two").css( "background-color", "white");
+			}, 1000
+		);
 	});
 
 	$( "#key-three").click(function() {
 		$( "#key-three").css( "background-color", "pink");
+		setTimeout(function(){ 
+			$( "#key-three").css( "background-color", "white");
+			}, 1000
+		);
 	});
 
 	$( "#key-four").click(function() {
 		$( "#key-four").css( "background-color", "pink");
+		setTimeout(function(){ 
+			$( "#key-four").css( "background-color", "white");
+			}, 1000
+		);
 	});
 
 	$( "#key-five").click(function() {
 		$( "#key-five").css( "background-color", "pink");
+		setTimeout(function(){ 
+			$( "#key-five").css( "background-color", "white");
+			}, 1000
+		);
 	});
 
 	$( "#key-six").click(function() {
 		$( "#key-six").css( "background-color", "pink");
+		setTimeout(function(){ 
+			$( "#key-six").css( "background-color", "white");
+			}, 1000
+		);
 	});
 
 	$( "#key-seven").click(function() {
 		$( "#key-seven").css( "background-color", "pink");
+		setTimeout(function(){ 
+			$( "#key-seven").css( "background-color", "white");
+			}, 1000
+		);
 	});
 
 	$( "#key-eight").click(function() {
 		$( "#key-eight").css( "background-color", "pink");
+		setTimeout(function(){ 
+			$( "#key-eight").css( "background-color", "black");
+			}, 1000
+		);
 	});
 
 	$( "#key-nine").click(function() {
 		$( "#key-nine").css( "background-color", "pink");
+		setTimeout(function(){ 
+			$( "#key-nine").css( "background-color", "black");
+			}, 1000
+		);
 	});
 
 	$( "#key-ten").click(function() {
 		$( "#key-ten").css( "background-color", "pink");
+		setTimeout(function(){ 
+			$( "#key-ten").css( "background-color", "black");
+			}, 1000
+		);
 	});
 
 	$( "#key-eleven").click(function() {
 		$( "#key-eleven").css( "background-color", "pink");
+		setTimeout(function(){ 
+			$( "#key-eleven").css( "background-color", "black");
+			}, 1000
+		);
 	});
 
 	$( "#key-twelve").click(function() {
 		$( "#key-twelve").css( "background-color", "pink");
+		setTimeout(function(){ 
+			$( "#key-twelve").css( "background-color", "black");
+			}, 1000
+		);
 	});
-
-	
-
-	function trigger() {};
 });
